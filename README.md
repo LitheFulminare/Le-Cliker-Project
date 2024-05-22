d1cf6f6b3f6e240f3f92c30d4893503ca5ef25b4
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-05-22 08:24:28 -0300

    misc

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Le Cliker Project
+
+funcao dentro de cada objeto q aumenta mais quanto mais tem
+
+> 5    *1.1
+5-10   *1.5
+10-20  *2
  
--- Scripts/Main/Hub.gd
+++ Scripts/Main/Hub.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var fly = 0
 var click_disabled = false
+var emp_cost = 50
 
 var employees = 1
 
@@ -23,11 +24,17 @@ func _input(event):
 			print(str(fly) + " moscas")
 
 	if Input.is_action_just_pressed("Buy"):
-		if fly >= 50:
-			fly -= 50
+		
+		var choice = "employee" # "choice" will be defined by which button the player presses
+								# in other words, by what the players chooses to buy
+		
+		if fly >= emp_cost:
+			fly -= emp_cost
+			emp_cost = int(emp_cost * 1.1)
 			employees += 1
 			print(str(employees) + " empregados")
 			print(str(fly) + " moscas")
+			print("próximo custa: " + str(emp_cost))
 		else: 
 			print("pobre kk")
 
